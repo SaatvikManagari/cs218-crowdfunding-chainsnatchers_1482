990e09b8cb44274d7049255e41f105c3f21adfa1
thchrn <charan@LAPTOP-7OESS8PF.localdomain>
2026-05-10 16:32:37 +0000

    Added hardhat config files files

---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  paths: {
+    sources:   "./contracts",
+    tests:     "./test",
+    cache:     "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
+export default config;
